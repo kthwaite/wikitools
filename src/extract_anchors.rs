a9085231000af291355c1868f7ed336a079aa8ec
cthwaite <cthwaite@penguinrandomhouse.co.uk>
2018-10-12 17:40:30 +0100

    Better filtering for anchor extraction

--- src/extract_anchors.rs
+++ src/extract_anchors.rs
@@ -2,8 +2,10 @@ use quick_xml::{
     self as qx,
     events::{Event, BytesStart}
 };
+use regex::Regex;
 use std::io::{BufReader, Read};
 
+pub struct Category(String);
 
 #[derive(Debug, Clone)]
 pub enum Anchor {
@@ -20,8 +22,12 @@ impl Anchor {
         match anchor.find("|") {
             Some(index) => {
                 let page = anchor[..index].to_owned();
-                let surface = anchor[index+1..].to_owned();
-                Anchor::Label{page, surface}
+                let surface = anchor[index+1..].trim();
+                if surface.is_empty() {
+                    Anchor::Direct(page)
+                } else {
+                    Anchor::Label{page, surface: surface.to_owned()}
+                }
             },
             None => Anchor::Direct(anchor.to_owned())
         }
@@ -31,12 +37,36 @@ impl Anchor {
     pub fn is_file(anchor: &str) -> bool {
         anchor.starts_with("File:") || anchor.starts_with("Image:")
     }
+
+    /// Check if an anchor points to Wiktionary.
+    pub fn is_wiktionary(anchor: &str) -> bool {
+        anchor.starts_with("wikt:") || anchor.starts_with("wiktionary:")
+    }
+
+    /// Check if an anchor points to Wikisource.
+    pub fn is_wikisource(anchor: &str) -> bool {
+        anchor.starts_with("s:")
+    }
+
+    /// Check if an anchor points to Wikiversity.
+    pub fn is_wikiversity(anchor: &str) -> bool {
+        anchor.starts_with("v:")
+    }
+
+    /// Check if an anchor points to handle.net.
+    pub fn is_handle(anchor: &str) -> bool {
+        anchor.starts_with("hdl:")
+    }
 }
 
 #[derive(Debug, Default, Clone)]
 pub struct Page {
     title: String,
     anchors: Vec<Anchor>
+}
+
+lazy_static! {
+    static ref EXT_LINK : Regex = Regex::new("^[A-Za-z]+:").unwrap();
 }
 
 impl Page {
@@ -47,11 +77,34 @@ impl Page {
         }
     }
 
-    fn extract_anchors(page: &str) -> Vec<Anchor> {
+    pub fn extract_categories(page: &str) -> Vec<Category> {
+        let page = match page.rfind("==References==") {
+            Some(index) => &page[index..],
+            None => page
+        };
         page.match_indices("[[")
             .filter_map(|(begin, _)| {
                 let initial = &page[begin + 2..];
-                if Anchor::is_file(initial) {
+                if initial.starts_with("Category:") {
+                    return initial.find("]]").and_then(|end| {
+                        Some(&initial[..begin + end])
+                    })
+                }
+                None
+            })
+            .map(|cat| Category(cat.to_owned()))
+            .collect::<Vec<_>>()
+    }
+
+    pub fn extract_anchors(page: &str) -> Vec<Anchor> {
+        let page = match page.rfind("==References==") {
+            Some(index) => &page[..index],
+            None => page
+        };
+        page.match_indices("[[")
+            .filter_map(|(begin, _)| {
+                let initial = &page[begin + 2..];
+                if EXT_LINK.is_match(initial) {
                     return None;
                 }
                 page[begin..].find("]]").and_then(|end| {
