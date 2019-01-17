525c278accdcb7c31f9c0bb1a9f7e478fecbf45e
cthwaite <cthwaite@gmail.co.uk>
2019-01-17 13:41:50 +0000

    open_bzip and open_seek_bzip take AsRef<Path>

--- src/utils.rs
+++ src/utils.rs
@@ -16,13 +16,13 @@ pub fn to_decode_buffer(file: File) -> BZipReader {
 }
 
 /// Open a bzip2 file.
-pub fn open_bzip(path: &Path) -> io::Result<BZipReader> {
+pub fn open_bzip<P: AsRef<Path>>(path: P) -> io::Result<BZipReader> {
     let file = File::open(path)?;
     Ok(to_decode_buffer(file))
 }
 
 /// Open a bzip2 multistream and seek to a zip file at a given index.
-pub fn open_seek_bzip(path: &Path, index: usize) -> io::Result<BZipReader> {
+pub fn open_seek_bzip<P: AsRef<Path>>(path: P, index: usize) -> io::Result<BZipReader> {
     let mut file = File::open(path)?;
     file.seek(SeekFrom::Start(index as u64))?;
     Ok(to_decode_buffer(file))
