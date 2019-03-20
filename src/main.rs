1c5da8f47094af8199432418b68175c152c37151
cthwaite <cthwaite@gmail.co.uk>
2019-03-20 19:24:28 +0000

    feat(main): Add utility functions for constructing Tries and FSTs

--- src/main.rs
+++ src/main.rs
@@ -10,35 +10,26 @@ mod surface_forms;
 mod template;
 mod utils;
 
-use std::fs::File;
-use std::io::{self, BufWriter};
-use std::path::Path;
-use std::sync::Mutex;
+use env_logger;
+use log::{debug, info};
+use std::io;
 
-use tantivy::{
-    collector::TopCollector, directory::MmapDirectory, query::QueryParser, schema::*, Index,
-};
-
-use crate::extract::{extract_with_writer, index_anchors};
+use crate::extract::extract_anchor_counts_to_trie;
+use crate::extract::{TrieBuilderFlat, TrieBuilderNested};
 use crate::indices::{read_indices, write_all_indices, write_template_indices, WikiDumpIndices};
-use crate::page::writer::{AnchorWriterJSONL, AnchorWriterTSV};
-use crate::redirect::write_redirects;
 use crate::settings::Settings;
-use crate::surface_forms::{extract_anchor_counts_from_anchors, write_anchor_counts};
 use crate::template::compile_templates;
-use crate::utils::mutex_bufwriter;
+use crate::utils::Timer;
 
-/// Build a Tantivy index from anchors in a wikipedia dump.
-fn build_index(
-    index_dir: &Path,
-    page_indices: &WikiDumpIndices,
-    data_dump: &Path,
-    buf_size: usize,
-) -> tantivy::Result<(Schema, Index)> {
-    let mut schema_builder = SchemaBuilder::default();
-    schema_builder.add_u64_field("id", FAST);
-    schema_builder.add_text_field("title", STRING | STORED);
-    schema_builder.add_text_field("links", TEXT | STORED);
+use bincode;
+use qp_trie::{
+    wrapper::{BStr, BString},
+    Trie,
+};
+use serde::Serialize;
+use std::fs::File;
+use std::io::{BufReader, BufWriter};
+use std::path::Path;
 
     let schema = schema_builder.build();
     let index = Index::create_in_dir(&index_dir, schema.clone()).unwrap();
@@ -67,60 +58,57 @@ fn dump_redirects(
     Ok(())
 }
 
-/// Dump page anchors to a JSONL file.
-fn dump_page_anchors(
-    page_indices: &WikiDumpIndices,
-    data_dump: &Path,
-    out_path: &Path,
-    buf_size: usize,
-) -> io::Result<()> {
-    let writer = mutex_bufwriter(out_path, buf_size)?;
-
-    extract_with_writer(AnchorWriterJSONL, &page_indices, &data_dump, &writer);
-    Ok(())
+/// Serialize a Trie into a .qpt binary file.
+fn write_to_qpt<V>(
+    anchor_counts: &Trie<BString, V>,
+    path: &Path,
+    buf_size: Option<usize>,
+) -> bincode::Result<()>
+where
+    V: Serialize,
+{
+    let file = File::create(path)?;
+    let buf_size = buf_size.unwrap_or(256 * 1024 * 1024);
+    let file = BufWriter::with_capacity(buf_size, file);
+    bincode::serialize_into(file, &anchor_counts)
 }
 
-/// Write anchors from a Wikipedia dump to text file.
-///
-/// # Arguments
-/// * `indices` - Parsed Wikipedia page indices for the corresponding dump
-/// * `dump` - Path to Wikipedia dump
-/// * `out_path` - Output path
-/// * `buf_size` - Buffer size for writer
-pub fn write_anchors(
-    indices: &WikiDumpIndices,
-    dump: &Path,
-    out_path: &Path,
-    buf_size: usize,
-) -> io::Result<()> {
-    let writer = mutex_bufwriter(out_path, buf_size)?;
+/// Build and serialise a FST from flat anchors.
+fn build_fst_from_anchors(anchor_counts_flat_path: &Path, output_path: Option<&Path>) -> Result<(), Box<std::error::Error>> {
+    let mut timer = Timer::new();
 
-    extract_with_writer(AnchorWriterTSV, &indices, &dump, &writer);
+    let output_path = output_path.unwrap_or(Path::new("anchor-counts.fst"));
+
+    info!("Loading anchor counts...");
+    timer.reset();
+    let file = File::open(anchor_counts_flat_path)?;
+    let reader = BufReader::with_capacity(256 * 1024 * 1024, file);
+    let anchor_counts: Trie<BString, u32> = bincode::deserialize_from(reader)?;
+    timer.finish();
+    
+    use fst::{Map, MapBuilder, IntoStreamer, Streamer};
+    use fst_regex::Regex;
+
+    info!("Stripping anchors...");
+    timer.reset();
+    let mut anchors = anchor_counts.into_iter()
+                .map(|(key, value)| (key.into(), value as u64))
+                .collect::<Vec<(String, u64)>>();
+    timer.finish();
+    info!("Sorting anchors...");
+    timer.reset();
+    anchors.sort_by(|(k1, _), (k2, _)| k1.partial_cmp(k2).unwrap());
+    timer.finish();
+
+    let file = File::create(output_path)?;
+    let buf = BufWriter::with_capacity(256 * 1024 * 1024, file);
+    let mut bld = MapBuilder::new(buf)?;
+    info!("Building FST...");
+    timer.reset();
+    bld.extend_iter(anchors.into_iter())?;
+    bld.finish().unwrap();
+    timer.finish();
     Ok(())
-}
-
-fn one_query(index: &Index, schema: &Schema, query: &str) {
-    index.load_searchers().unwrap();
-
-    let searcher = index.searcher();
-    let (title, links) = (
-        schema.get_field("title").unwrap(),
-        schema.get_field("links").unwrap(),
-    );
-    let query_parser = QueryParser::for_index(&index, vec![links]);
-    let query = query_parser.parse_query(query).unwrap();
-
-    let mut top_collector = TopCollector::with_limit(10);
-
-    searcher.search(&*query, &mut top_collector).unwrap();
-    let doc_addresses = top_collector.docs();
-
-    for doc_address in doc_addresses {
-        let retrieved_doc = searcher.doc(doc_address).unwrap();
-        if let Some(doc_title) = retrieved_doc.get_first(title) {
-            println!("{:?}", doc_title);
-        }
-    }
 }
 
 fn main() -> Result<(), Box<std::error::Error>> {
