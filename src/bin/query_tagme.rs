f37ce5b7fd22a45f7a93bc84b447e0076c497451
cthwaite <cthwaite@gmail.co.uk>
2019-03-23 13:41:08 +0000

    feat(query_tagme): Command-line bin for TAGME queries

--- src/bin/query_tagme.rs
+++ src/bin/query_tagme.rs
@@ -1,31 +1,41 @@
 use tagme::TagMeQuery;
 
-use log::info;
-use tantivy::{directory::MmapDirectory, schema::*, Index};
+use clap::{App, Arg};
 
 use storage::fst::WikiAnchors;
 use storage::tantivy::TantivyWikiIndex;
 
-pub fn create_schema() -> Schema {
-    let mut schema_builder = SchemaBuilder::default();
-
-    schema_builder.add_u64_field("id", FAST);
-    schema_builder.add_text_field("title", STRING | STORED);
-    schema_builder.add_text_field("content", TEXT);
-
-    schema_builder.build()
-}
-
-fn main() {
+fn main() -> Result<(), Box<std::error::Error>> {
     use env_logger;
     env_logger::init();
-    info!("Running query");
+
+    let app = App::new("query_tagme")
+        .version("0.0.0")
+        .about("Parse and return entities for a text fragment.")
+        .arg(
+            Arg::with_name("query")
+                .takes_value(true)
+                .help("Query to tag entities for")
+                .required(true),
+        )
+        .get_matches();
+
+    let query = match app.value_of("query") {
+        Some(query) => query,
+        None => {
+            println!("{}", app.usage());
+            return Ok(());
+        }
+    };
 
     let map = WikiAnchors::new("./anchors-flat.fst").unwrap();
-    let index = TantivyWikiIndex::new("./wiki-index");
+    let index = TantivyWikiIndex::new("./wiki-index-with-links");
 
-    let mut qry = TagMeQuery::new("Orpheus and Eurydice", 1.0);
+    let mut qry = TagMeQuery::new("The museum is housed in the Louvre Palace, originally built as the Louvre castle in the late 12th to 13th century under Philip II", 1.0);
     let ents = qry.parse(&map, &index);
-    let ents = qry.disambiguate(&ents);
-    println!("{:?}", ents);
+    println!("before disambiguation: {:?}", ents);
+    let ents = qry.disambiguate(&index, &ents);
+    println!("========================================\n\n");
+    println!("final: {:?}", ents);
+    Ok(())
 }
